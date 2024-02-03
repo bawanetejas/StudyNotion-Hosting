2caaab18de7ac5f9a33149094957227a624d763a
bawanetejas <tejasbawane221@gmail.com>
2024-02-04 01:41:24 +0530

    cors options chang BE

--- server/index.js
+++ server/index.js
@@ -28,8 +28,9 @@ app.use(cookieParser())
 
 app.use(
     cors({
-        origin:"http://localhost:3000",
-        credentials:true,
+        origin:'http://localhost:3000', 
+        credentials:true,           
+        optionSuccessStatus:200
     })
 )
 
